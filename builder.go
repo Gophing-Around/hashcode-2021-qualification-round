057a09e248a3762c8200cb317a9be51bb7cdd352
Federico Maggi <federico.maggi@mia-platform.eu>
2021-02-25 19:02:47 +0100

    feat: parser

--- builder.go
+++ builder.go
@@ -1,7 +1,63 @@
 package main
 
-func buildInput(inputSet string) int {
-	return 42
+type Config struct {
+	simuDuration  int
+	intersections int
+	nStreets      int
+	nCars         int
+	bonusPoints   int
+}
+
+func buildConfig(inputSet string) Config {
+	parts := splitSpaces(inputSet)
+	return Config{
+		simuDuration:  toint(parts[0]),
+		intersections: toint(parts[1]),
+		nStreets:      toint(parts[2]),
+		nCars:         toint(parts[3]),
+		bonusPoints:   toint(parts[4]),
+	}
+}
+
+type Street struct {
+	startIntersection int
+	endIntersection   int
+	name              string
+	timeNeeded        int
+}
+
+type CarsPaths struct {
+	nStreets    int
+	streetNames []string
+}
+
+func buildStreets(c Config, lines []string) []Street {
+	streets := make([]Street, c.nStreets)
+
+	for i := 0; i < c.nStreets; i++ {
+		parts := splitSpaces(lines[i])
+		streets[i] = Street{
+			startIntersection: toint(parts[0]),
+			endIntersection:   toint(parts[1]),
+			name:              parts[2],
+			timeNeeded:        toint(parts[3]),
+		}
+	}
+	return streets
+}
+
+func buildCarsPaths(c Config, lines []string) []CarsPaths {
+	buildCars := make([]CarsPaths, c.nCars)
+
+	for i := 0; i < c.nCars; i++ {
+		parts := splitSpaces(lines[i])
+		buildCars[i] = CarsPaths{
+			nStreets:    toint(parts[0]),
+			streetNames: parts[1:],
+		}
+	}
+
+	return buildCars
 }
 
 func buildOutput(result int) string {
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 
 	"github.com/fredmaggiowski/gowq"
 )
@@ -11,9 +10,9 @@ func main() {
 	files := []string{
 		// Uncomment the line with the desired files (add other lines if needed)
 		"a",
-		// "a", "b", "c", "d", "e", "f",
-		// "a", "b",
-		// "a", "b", "e", "f",
+		// "a",
+		// "a",
+		// "a",
 		// "c",
 		// "d",
 	}
@@ -22,16 +21,24 @@ func main() {
 
 	for _, fileName := range files {
 		fmt.Printf("****************** INPUT: %s\n", fileName)
-		inputSet := readFile(fmt.Sprintf("./inputFiles/%s.in", fileName))
+		inputSet := readFile(fmt.Sprintf("./inputFiles/%s.txt", fileName))
 
-		input := buildInput(inputSet)
-		printInputMetrics(input)
+		fileLines := splitNewLines(inputSet)
 
-		result := algorithm(input)
+		config := buildConfig(fileLines[0])
+		// fmt.Printf("%+v\n", config)
 
-		output := buildOutput(result)
-		printResultMetrics(result)
+		streets := buildStreets(config, fileLines[1:])
+		// fmt.Printf("%+v\n", streets)
+		carsPaths := buildCarsPaths(config, fileLines[1+config.nStreets:])
+		// fmt.Printf("%+v\n", carsPaths)
 
-		ioutil.WriteFile(fmt.Sprintf("./result/%s.out", fileName), []byte(output), 0644)
+		// printInputMetrics(input)
+		// result := algorithm(input)
+
+		// output := buildOutput(result)
+		// printResultMetrics(result)
+
+		// ioutil.WriteFile(fmt.Sprintf("./result/%s.txt", fileName), []byte(output), 0644)
 	}
 }
